3a879fee38979fd005c045c6531840f78a7bd14d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-07 10:31:50 -0800

    added toggleComplete method to todos.js

--- imports/api/todos.js
+++ imports/api/todos.js
@@ -1,3 +1,21 @@
 import { Mongo } from "meteor/mongo";
 
+// all methods specific to todos
+// meteor asks that methods be defined as a string. 
+// structure = <collection>.<whatever we want method to be named>
+Meteor.methods({
+
+    // Adding a todo
+
+    // Toggling complete (update)
+    'todos.toggleComplete'
+
+    // Removing a todo
+
+    // Removing all completed todos
+
+
+})
+
+
 export const ToDos = new Mongo.Collection("todos");
