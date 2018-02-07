8c9f6231b1a3ba464e2888c737c5b55c93347240
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-07 10:12:33 -0800

    added a default user check

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,8 +1,15 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 import { ToDos } from "../../api/todos";
 
 Meteor.startup(() => {
-  if (ToDos.find().count() === 0) {
-    ToDos.insert({ title: "Learn Meteor Again", complete: false });
+  if (Meteor.users.find().count() === 0) {
+    let userId = Accounts.createUser({
+      email: "nicholas@nicholas.com",
+      password: "pass"
+    });
+    if (ToDos.find().count() === 0) {
+      ToDos.insert({ title: "Learn Meteor Again", complete: false });
+    }
   }
 });
