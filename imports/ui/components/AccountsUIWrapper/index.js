2d8b6bccc42a58ac251954ba2b001f170080fc51
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-07 09:27:39 -0800

    added bcrypt dependencies and packages to .meteor/packages

--- imports/ui/components/AccountsUIWrapper/index.js
+++ imports/ui/components/AccountsUIWrapper/index.js
@@ -4,6 +4,7 @@ import { Template } from "meteor/templating";
 import { Blaze } from "meteor/blaze";
 
 import "./styles.css";
+
 export default class AccountsUIWrapper extends Component {
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
@@ -15,7 +16,6 @@ export default class AccountsUIWrapper extends Component {
   componentWillUnmount() {
     Blaze.remove(this.view); // Clean up Blaze view
   }
-
   render() {
     return <span ref="container" />; // Render a placeholder
   }
